953ef9943d15cfb25de0fe9a5b79d9e7e47bceac
Carbon225 <carbon225@protonmail.com>
2022-07-31 03:00:34 +0200

    remove label from tokenizer

--- transforchess/model/gpt2clm/tokenizer.py
+++ transforchess/model/gpt2clm/tokenizer.py
@@ -10,20 +10,13 @@ def tokenize_dataset():
     tokenizer = AutoTokenizer.from_pretrained(BASE_CHECKPOINT)
 
     def tokenize(element):
-        if element['label'] == '1-0':
-            element['label'] = 0
-        elif element['label'] == '0-1':
-            element['label'] = 1
-        else:
-            element['label'] = 2
-        element = tokenizer(element['text'], truncation=True)
-        return element
+        return tokenizer(element['text'], truncation=True)
 
     tokenized_dataset = dataset.map(
         tokenize,
         batched=False,
         num_proc=24,
-        remove_columns=['text'],
+        remove_columns=dataset.column_names,
     )
 
     tokenized_dataset.save_to_disk(TOKENIZED_DATASET)
