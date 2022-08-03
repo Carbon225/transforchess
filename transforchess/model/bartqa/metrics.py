b4d907d11b557f7e36e915c55d792ee26b879112
Carbon225 <carbon225@protonmail.com>
2022-08-03 10:37:55 +0200

    batched game length metric

--- transforchess/model/bartqa/metrics.py
+++ transforchess/model/bartqa/metrics.py
@@ -1,8 +1,9 @@
 import numpy as np
+from random import choice
 from chess import Board
 from transformers import pipeline
 
-from transforchess.parser import human2san
+from transforchess.parser import human2san, san2human
 
 
 class GameLength:
@@ -13,20 +14,33 @@ class GameLength:
             'mean': np.mean,
         }[method]
 
-    def __call__(self, model, tokenizer) -> int:
+    def __call__(self, model, tokenizer) -> float:
         task = pipeline('text2text-generation', model=model, tokenizer=tokenizer)
-        lengths = [self._run_game(task) for _ in range(self.n_games)]
-        return self.method(lengths)
 
-    def _run_game(self, task) -> int:
-        board = Board()
-        game = ''
-        while not board.is_game_over():
-            try:
-                move_human = task(game)[0]['generated_text']
-                move_san = human2san(move_human[7:-1])
-                board.push_san(move_san)
-                game += move_human
-            except Exception:
-                break
-        return len(board.move_stack)
+        boards = [Board() for _ in range(self.n_games)]
+        games = ['' for _ in range(self.n_games)]
+        is_over = [False] * self.n_games
+
+        while not all(is_over):
+            inds = [i for i, over in enumerate(is_over) if not over]
+
+            sequences = task([games[i] for i in inds])
+
+            for i, sequence in enumerate(sequences):
+                try:
+                    move_human = sequence['generated_text']
+                    move_san = human2san(move_human[7:-1])
+                    boards[inds[i]].push_san(move_san)
+                    games[inds[i]] += move_human
+
+                    move = choice(list(boards[inds[i]].legal_moves))
+                    san = boards[inds[i]].san(move)
+                    boards[inds[i]].push(move)
+                    games[inds[i]] += f' Black {san2human(san)}.'
+
+                    if boards[inds[i]].is_game_over():
+                        is_over[inds[i]] = True
+                except Exception:
+                    is_over[inds[i]] = True
+
+        return self.method([len(board.move_stack) for board in boards])
